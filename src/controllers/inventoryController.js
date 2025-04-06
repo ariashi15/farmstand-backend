706a207d5cdd682081da45f67b917c0bd397b9f0
Aria Shi <ariashi15@gmail.com>
2025-04-06 03:12:09 -0500

    getInventoryByFarmID endpoint

--- src/controllers/inventoryController.js
+++ src/controllers/inventoryController.js
@@ -30,6 +30,37 @@ const inventoryController = {
         }
     },
 
+    async getInventoryByFarmID(req, res) {
+        try {
+            const { farmid } = req.params;
+            const { data, error } = await supabase
+                .from('inventory')
+                .select(`
+                    item_id,
+                    farm_id,
+                    item_name,
+                    image_url,
+                    price,
+                    unit,
+                    quantity,
+                    farms ( farm_id, name, pickup, delivery )
+                    `)
+                .eq('farm_id', farmid);
+
+            if (error) {
+                console.log(error.message);
+                return res.status(400).json({ error: error.message });
+            };
+
+            if (data) {
+                return res.json(data);
+            };
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    },
+
     async getItemByID(req, res) {
         try {
             // extract the item id
--- src/routes/inventoryRoutes.js
+++ src/routes/inventoryRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/allfarms', inventoryController.getAllFarmsInventory);
 router.get('/:productid', inventoryController.getItemByID);
+router.get('/farm/:farmid', inventoryController.getInventoryByFarmID);
 
 module.exports = router;
